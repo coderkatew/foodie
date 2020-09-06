ded2e90a0300e6690811d5a2a299ea960e10cd87
coderkatew <kateat20@yahoo.co.uk>
2020-09-06 21:22:57 +0000

    Connect to MongoDB and create database

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 # creates instance of Flash
 app = Flask(__name__)
 
+app.config["MONGODB_NAME"] = 'foodie'
+app.config["MONGODB_URI"] = 'mongodb+srv://root:<password>@firstcluster.gtfgd.mongodb.net/<foodie>?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
+
 def hello():
     return ('Hello World ...again')
 
