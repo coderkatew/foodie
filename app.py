d9faf0bb6716a5650e302707d29f354ccdf70b19
coderkatew <kateat20@yahoo.co.uk>
2020-09-07 20:48:17 +0000

    Add hidden database login

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 # creates instance of Flash
 app = Flask(__name__)
 
-app.config["MONGODB_NAME"] = 'foodie'
-app.config["MONGODB_URI"] = 'mongodb+srv://root:<password>@firstcluster.gtfgd.mongodb.net/<foodie>?retryWrites=true&w=majority'
+app.config["MONGODB_NAME"] = os.environ.get('MONGODB_URI')
+app.config["MONGODB_URI"] = os.environ.get('MONGODB_URI')
 
 mongo = PyMongo(app)
 
-@app.route('/')
 
-def hello():
-    return ('Hello World ...again')
+@app.route('/')
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("index.html", recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
