f18454c5e17a0f31b895b8167841ec13034fd832
coderkatew <kateat20@yahoo.co.uk>
2020-09-13 12:37:33 +0000

    Add dropdown menus to add recipe form

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import (Flask, render_template, redirect, request, url_for, session, flash)
+from flask import (Flask, render_template, redirect,
+                   request, url_for, session, flash)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -21,18 +22,25 @@ mongo = PyMongo(app)
 @app.route('/all_recipes')
 def all_recipes():
     recipes = mongo.db.recipes.find()
-    return render_template('all_recipes.html', recipes = recipes)
+    return render_template('all_recipes.html', recipes=recipes)
+
 
 # add a new recipe
 @app.route('/add_recipe')
 def add_recipe():
-    return render_template('add_recipe.html')
+    return render_template('add_recipe.html',
+                           title="Add Recipe",
+                           categories=mongo.db.categories.find(),
+                           levels=mongo.db.levels.find(),
+                           cuisines=mongo.db.cuisines.find())
+
 
 # browse recipes
 @app.route('/browse_recipes')
 def browse_recipes():
     recipes = mongo.db.recipes.find()
-    return render_template('browse_recipes.html', recipes = recipes)
+    return render_template('browse_recipes.html', recipes=recipes)
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
